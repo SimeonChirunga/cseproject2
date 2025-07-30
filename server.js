c17d7ff95df8d2a6ed39652e394f64779b80df71
simeonchirunga <schirunga1@byupathway.edu>
2025-07-30 13:38:35 +0200

    updated cors

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS (before session/auth)
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: process.env.CLIENT_URL || 'https://localhost:3000',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
